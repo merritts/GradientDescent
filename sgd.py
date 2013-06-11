3ef0f5a003fa160e1633acba524c6c61e6f1e07c
Sears Merritt <sears.merritt@gmail.com>
2013-06-10 19:44:47 -0600

    stochastic gradient descent, removed regularized method

--- sgd.py
+++ sgd.py
@@ -3,7 +3,6 @@ from array import array
 
 import numpy as np
 
-
 def h(x,theta):
     return 1./(1.+e**(np.dot(-x,theta)))
 
@@ -27,38 +26,6 @@ def log_reg_sgd(x,y,a,ep=0.0000001,max_iter=100000,debug=True):
         for i in z:
             #update weights
             theta = theta + a*(y[i]-h(x[i],theta))*x[i]
-            #compute the error and test for convergence
-            e = sum([(y[i]-h(x[i],theta))**2 for i in range(m)])
-            if debug: err.append(e)
-            if abs(pe-e) <= ep:
-                cvg = True
-                break
-            pe = e
-        iters+=1
-        if iters == max_iter:
-            print '*** Max interactions exceeded ***'
-            break
-    if debug:
-        return theta,err
-    return theta
-
-
-def log_reg_regularized_sgd(x,y,a,ep=0.0000001,max_iter=100000,debug=True):
-    iters=0
-    if debug: err = array('f',[])
-    ### initialize algorithm state ###
-    m,n = x.shape
-    cvg = False
-    theta = np.random.random(n)
-    z = np.arange(m)
-    pe = sum([(y[i]-h(x[i],theta))**2 for i in range(m)])
-    ### update rule ###
-    while not cvg:
-        #shuffle indices prior to searching
-        z = np.random.permutation(z)
-        for i in z:
-            #update weights
-            theta = theta + a*(y[i]-h(x[i],theta))*x[i]+l/m*theta
             #compute the error and test for convergence
             e = sum([(y[i]-h(x[i],theta))**2 for i in range(m)])
             if debug: err.append(e)
