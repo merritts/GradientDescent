1c522ec64ee5284dc0d3ec1e174b07f0d46e0bf1
Sears Merritt <sears.merritt@gmail.com>
2013-07-27 20:51:59 -0600

    fixed update rule for regularized sgd

--- sgd.py
+++ sgd.py
@@ -10,9 +10,11 @@ def h(x,theta):
     """
     return e**(np.dot(x,theta))/(1.+e**(np.dot(x,theta)))
 
-def log_reg_sgd(x,y,a,max_iter=1000,debug=True):
+
+def log_reg_sgd(x,y,a,max_iter=100,debug=True):
     """
-    Stochastic gradient descent for logistic regression, static learning rate
+    Stochastic gradient descent for two class (0,1) logistic regression
+    with static learning rate
     """
     if debug: err = array('f',[])
     ### initialize algorithm state ###
@@ -20,7 +22,6 @@ def log_reg_sgd(x,y,a,max_iter=1000,debug=True):
     theta = np.random.random(n)
     z = np.arange(m)
     for t in xrange(max_iter):
-        print t
         #shuffle indices prior to searching
         z = np.random.permutation(z)
         #for each training example
@@ -33,38 +34,24 @@ def log_reg_sgd(x,y,a,max_iter=1000,debug=True):
     return theta
 
 
-def log_reg_regularized_sgd(x,y,a,l=0.1,ep=0.00001,max_iter=100000,debug=True):
+def log_reg_regularized_sgd(x,y,a,l=0.1,max_iter=100,debug=True):
     """
     Stochastic gradient descent for logistic regression with regularization
+    and static learning rate
     """
-    iters=0
     if debug: err = array('f',[])
     ### initialize algorithm state ###
     m,n = x.shape
-    p = 1.-a*l/m
-    cvg = False
-    #theta = np.random.random(n)
-    theta = np.zeros(n)
+    theta = np.random.random(n)
     z = np.arange(m)
-    pe = sum([(y[i]-h(x[i],theta))**2 for i in range(m)])
-    ### update rule ###
-    while not cvg:
+    for t in xrange(max_iter):
         #shuffle indices prior to searching
         z = np.random.permutation(z)
+        #for each training example
         for i in z:
             #update weights
-            theta = theta*p - a*(h(x[i],theta)-y[i])*x[i]
+            theta = theta + a*(y[i]-h(x[i],theta))*x[i] - l*2.*a*theta
             #compute the error and test for convergence
-            e = sum([(y[i]-h(x[i],theta))**2 for i in range(m)])
-            if debug: err.append(e)
-            if abs(pe-e) <= ep:
-                cvg = True
-                break
-            pe = e
-        iters+=1
-        if iters == max_iter:
-            print '*** Max interactions exceeded ***'
-            break
-    if debug:
-        return theta,err
+            if debug:err.append(sum([(y[i]-h(x[i],theta))**2 for i in range(m)]))
+    if debug: return theta,err
     return theta
