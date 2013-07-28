0e5f19b459c01bca5a64d3965a91af1366b75651
Sears Merritt <sears.merritt@gmail.com>
2013-07-27 20:28:41 -0600

    add logistic regression with fixed learning rate

--- sgd.py
+++ sgd.py
@@ -2,18 +2,46 @@ from math import e
 from array import array
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def h(x,theta):
-    return 1./(1.+e**(np.dot(-x,theta)))
-
-def log_reg_sgd(x,y,a,ep=0.0000001,max_iter=100000,debug=True):
     """
-    Stochastic gradient descent for logistic regression
+    logistic regression hypothesis function
+    """
+    return e**(np.dot(x,theta))/(1.+e**(np.dot(x,theta)))
+
+def log_reg_sgd(x,y,a,max_iter=1000,debug=True):
+    """
+    Stochastic gradient descent for logistic regression, static learning rate
+    """
+    if debug: err = array('f',[])
+    ### initialize algorithm state ###
+    m,n = x.shape
+    theta = np.random.random(n)
+    z = np.arange(m)
+    for t in xrange(max_iter):
+        print t
+        #shuffle indices prior to searching
+        z = np.random.permutation(z)
+        #for each training example
+        for i in z:
+            #update weights
+            theta = theta + a*(y[i]-h(x[i],theta))*x[i]
+            #compute the error and test for convergence
+            if debug:err.append(sum([(y[i]-h(x[i],theta))**2 for i in range(m)]))
+    if debug: return theta,err
+    return theta
+
+
+def log_reg_regularized_sgd(x,y,a,l=0.1,ep=0.00001,max_iter=100000,debug=True):
+    """
+    Stochastic gradient descent for logistic regression with regularization
     """
     iters=0
     if debug: err = array('f',[])
     ### initialize algorithm state ###
     m,n = x.shape
+    p = 1.-a*l/m
     cvg = False
     #theta = np.random.random(n)
     theta = np.zeros(n)
@@ -25,7 +53,7 @@ def log_reg_sgd(x,y,a,ep=0.0000001,max_iter=100000,debug=True):
         z = np.random.permutation(z)
         for i in z:
             #update weights
-            theta = theta + a*(y[i]-h(x[i],theta))*x[i]
+            theta = theta*p - a*(h(x[i],theta)-y[i])*x[i]
             #compute the error and test for convergence
             e = sum([(y[i]-h(x[i],theta))**2 for i in range(m)])
             if debug: err.append(e)
